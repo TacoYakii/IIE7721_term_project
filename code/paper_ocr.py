07f372fbb830a1d2e60ef8d170b91dc7cec157ec
tacoyakii <1227peter@yonsei.ac.kr>
2025-05-08 21:04:09 +0900

    Seperated pages (OCR)

--- code/paper_ocr.py
+++ code/paper_ocr.py
@@ -4,7 +4,10 @@ import os
 import easyocr
 from tqdm import tqdm 
 
-
+def create_dir(path): 
+    if not os.path.exists(path): 
+        os.makedirs(path) 
+    
 def convert_to_image(pdf_path:str, sv_root:str) -> None: 
     """
     Convert every pdf files to image files from given pdf_path
@@ -19,8 +22,10 @@ def convert_to_image(pdf_path:str, sv_root:str) -> None:
         with open(pdf, "rb") as f: 
             img = pdf2image.convert_from_bytes(f.read())
         img_nm = os.path.basename(pdf).split(".")[0] 
-        sv_path = os.path.join(sv_root, f"{img_nm}.jpg") 
-        img[0].save(sv_path, "JPEG")
+        sv_path = os.path.join(sv_root, f"{img_nm}") 
+        create_dir(sv_path) 
+        for i in range(len(img)): 
+            img[i].save(os.path.join(sv_path, f"{i}.jpg"), "JPEG")
 
 
 def image_to_txt(img_path:str, sv_root:str) -> None: 
@@ -31,26 +36,25 @@ def image_to_txt(img_path:str, sv_root:str) -> None:
         img_path (str): image root path 
         sv_path (str): save root path 
     """
+    create_dir(sv_root)
     reader = easyocr.Reader(
         ["en", "ko"], 
         gpu=True 
     )
-    img_dir_list = list(glob(f"{img_path}/*.jpg"))
+    img_dir_list = list(glob(f"{img_path}/*"))
     for img_dir in tqdm(img_dir_list, desc="converting image to txt"): 
-        with open(img_dir, "rb") as f: 
-            img = f.read()
-        result = reader.readtext(
-            img, 
-            detail=0, 
-            paragraph=True
-            )
-        img_nm = os.path.basename(img_dir).split(".")[0] 
-        sv_path = os.path.join(sv_root, f"{img_nm}.txt") 
-        with open(sv_path, "w", encoding="utf-8") as f: 
-            for line in result: 
-                words = line.split() 
-                f.write("\n".join(words)+"\n") 
-                
+        file_nm = os.path.basename(img_dir).split(".")[0] 
+        img_list = list(glob(f"{img_dir}/*.jpg")) 
+        create_dir(os.path.join(sv_root, file_nm))
+        for page in img_list: 
+            page_no = os.path.basename(page).split(".")[0] 
+            with open(page, "rb") as f: 
+                page_img = f.read() 
+            page_str = reader.readtext(page_img, detail=0, paragraph=True) 
+            with open(os.path.join(sv_root, file_nm, f"{page_no}.txt"), "w", encoding="utf-8") as txt_file: 
+                for line in page_str: 
+                    txt_file.write(line)
+
 
 original_pdf_path = "/home/taco/Documents/projects/paper_analysis/data/original" 
 image_sv_root = "/home/taco/Documents/projects/paper_analysis/data/image"
